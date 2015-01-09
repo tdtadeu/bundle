1a9f93979c21e8e3ae3d11f48525fe76b861c645
Thiago Dias <tdtadeu@gmail.com>
2015-01-09 16:03:57 -0200

    Added libxml2-dev before bundle

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,10 +1,23 @@
 def install(users)
+  install_libxml2_dev
+
   users.each do |user|
-    execute "bundle-install-#{user}" do
-      action :run
-      command "bundle install"
-      cwd "/vagrant"
-    end
+    run_bundle_install(user)
+  end
+end
+
+def install_libxml2_dev
+  package "libxml2-dev" do
+    options "--force-yes"
+    action :upgrade
+  end
+end
+
+def run_bundle_install(user)
+  execute "bundle-install-#{user}" do
+    action :run
+    command "bundle install"
+    cwd "/vagrant"
   end
 end
 
